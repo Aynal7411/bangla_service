bcce71030f1925f021a90cd26769701b37c94ee9
Aynal Haque <aynalhaque7411@gmail.com>
2025-05-09 06:48:30 +0000

    update

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_home_page, name='home_page'),
+    path('post/', views.post_list, name='post_list'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def landing_home_page(request):
     return render(request, 'core/landing_page.html')
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published=True).order_by('-created_at')
+    return render(request, 'core/post_list.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, published=True)
+    return render(request, 'core/post_detail.html', {'post': post})
